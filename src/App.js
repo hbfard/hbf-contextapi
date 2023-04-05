87d4a40288892351ce21db279c1ac66c0c7f6864
HFA <HFA@HFA-LB-22.m.mdv>
2023-04-05 17:50:48 +0200

    With product service

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import AppContent from "./components/AppContent";
 import AppFooter from "./components/AppFooter";
 import AppHeader from "./components/AppHeader";
 
+import { getProduct } from "./services/Products";
+
+const products = [...Array(10)].map(() => getProduct());
+
+console.log(products);
+
 function App() {
   return (
     <Layout>
